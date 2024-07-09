82e0c8a69a22524b4cde431d17857e21d04db8cc
Tristan Saidi <tls2160@columbia.edu>
2024-07-09 10:34:53 -0400

    add an isomap wrapper that deals with >1 cc

--- src/embeddings.py
+++ src/embeddings.py
@@ -81,6 +81,47 @@ def spectral_embedding(A, n_components):
     se = manifold.SpectralEmbedding(n_components=n_components, affinity='precomputed')
     Y = se.fit_transform(A)
     return Y
+
+def isomap_connected_component(A, n_components, X=None):
+    """
+    Compute the Isomap embedding of a graph with (potentially) > 1 connected component.
+    Parameters
+    ----------
+    A : array-like, shape (n_samples, n_samples)
+        The adjacency matrix of the graph.
+    n_components : int
+        The number of components to keep.
+    X : array-like, shape (n_samples, n_features), default=None
+        Unused.
+    Returns
+    -------
+    Y : array-like, shape (n_samples, n_components)
+        The Isomap embedding of the graph.
+    """
+    n_connected_components, component_labels = scipy.sparse.csgraph.connected_components(A)
+    Y = []
+    # separately embed each connected component        
+    for i in range(n_connected_components):
+        idx_i = np.flatnonzero(component_labels == i)
+        print('Connected component:', i, 'Number of points:', len(idx_i))
+        # skip really small connected components
+        if len(idx_i) < A.shape[0] // (n_connected_components * 2):
+            continue
+        Ai = A[np.ix_(idx_i, idx_i)]
+        assert np.allclose(Ai, Ai.T), "The adjacency matrix is not symmetric."
+        Yi = isomap(Ai, n_components)
+        Y.append(Yi)
+
+    num_clusters = len(Y)
+    print('Number of clusters:', num_clusters)
+    if num_clusters > 1:
+        for cluster in range(num_clusters):
+            theta = 2 * np.pi * cluster / num_clusters
+            centroid = 3 * np.array([np.cos(theta), np.sin(theta)])
+            Y[cluster] += centroid
+
+    Y = np.concatenate(Y)
+    return Y
     
 def isomap(A, n_components, X=None):
     """
@@ -99,39 +140,12 @@ def isomap(A, n_components, X=None):
         The Isomap embedding of the graph.
     """
 
-    n_connected_components, component_labels = scipy.sparse.csgraph.connected_components(A)
-    
-    if n_connected_components > 1:
-        if X is None:
-            raise ValueError("The graph is not connected. Please provide the original data.")
-        warnings.warn(
-            (
-                "The number of connected components of the neighbors graph "
-                f"is {n_connected_components} > 1. Completing the graph to fit"
-                " Isomap might be slow. Increase the number of neighbors to "
-                "avoid this issue."
-            ),
-            stacklevel=2,
-        )
-        # use array validated by NearestNeighbors
-        ambient_distances = scipy.spatial.distance.pdist(X, metric="euclidean")
-        ambient_distances = scipy.spatial.distance.squareform(ambient_distances)
-
-        A = _fix_connected_components(
-            X=A,
-            graph=ambient_distances,
-            component_labels=component_labels,
-            n_connected_components=n_connected_components,
-            mode="distance",
-            metric="precomputed",
-        )
-
-
+    n_connected_components, _ = scipy.sparse.csgraph.connected_components(A)
+    assert n_connected_components == 1, "The graph is not connected."
     # isomap with precomputed distances
     iso = Isomap(metric='precomputed', n_components=n_components)
     # compute geodesic distances
     distances = scipy.sparse.csgraph.shortest_path(A, directed=False)
-    assert scipy.sparse.csgraph.connected_components(A)[0] == 1, "The graph is not connected."
     assert np.allclose(distances, distances.T), "The from scipy.sparse.csgraph import connected_componentsdistance matrix is not symmetric."
     
     Y = iso.fit_transform(distances)
